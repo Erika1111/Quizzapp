6892fd5bde907f917415f5d593ef7341c64d944e
Erika Linde <erika.j.linde@gmail.com>
2017-01-17 14:54:22 +0100

    Initial Commit

---
+++ Quizapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quizapp
+//
+//  Created by Erika Linde on 17/01/17.
+//  Copyright © 2017 Erika Linde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Quizapp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Quizapp
+//
+//  Created by Erika Linde on 17/01/17.
+//  Copyright © 2017 Erika Linde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
